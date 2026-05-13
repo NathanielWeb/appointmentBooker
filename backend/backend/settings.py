686d71a1ee9eba37ba30c8a017ce2df1178973fc
webst149 <nathaniel.webster@mail.utoronto.ca>
2026-05-13 06:08:11 -0400

    small fix needed for deployment

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -27,7 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config("SECRET_KEY")
 DEBUG = config("DEBUG", cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    config("HOST_ONE")
+]
 
 
 # Application definition
